a33649e5cf986d9732ca65b212bd44305819ad77
Valentin Calomino <vcalomino@fi.uba.ar>
2024-10-25 10:25:14 -0300

    remove unused import

--- src/main/java/com/ing_software/tp/service/UserService.java
+++ src/main/java/com/ing_software/tp/service/UserService.java
@@ -3,7 +3,6 @@ package com.ing_software.tp.service;
 import com.ing_software.tp.dto.UserRegisterRequest;
 import com.ing_software.tp.model.User;
 import com.ing_software.tp.repository.UserRepository;
-import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 
 @Service
