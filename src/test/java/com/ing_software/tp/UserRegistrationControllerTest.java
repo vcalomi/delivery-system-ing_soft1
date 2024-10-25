ef22f35ffa5a50b82dfd117ddf020009289917e2
Valentin Calomino <vcalomino@fi.uba.ar>
2024-10-25 20:09:57 -0300

    add in memory database for tests

--- src/test/java/com/ing_software/tp/UserRegistrationControllerTest.java
+++ src/test/java/com/ing_software/tp/UserRegistrationControllerTest.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@TestPropertySource("classpath:application-test.properties")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserRegistrationControllerTest {
 
