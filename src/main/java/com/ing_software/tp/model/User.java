148f73c2ded4b1cbebf8aaa871e9b5f533d94ba9
Valentin Calomino <vcalomino@fi.uba.ar>
2024-10-25 00:07:20 -0300

    add lombok

--- src/main/java/com/ing_software/tp/model/User.java
+++ src/main/java/com/ing_software/tp/model/User.java
@@ -1,9 +1,15 @@
 package com.ing_software.tp.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "app_user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -14,56 +20,4 @@ public class User {
     private String email;
     private int age;
     private String address;
-
-
-
-    public User(){}
-
-    public User(String name, String lastname, String email, int age, String address ){
-        this.name = name;
-        this.lastname = lastname;
-        this.email = email;
-        this.age = age;
-        this.address = address;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
