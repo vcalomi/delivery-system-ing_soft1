a292784e137d6aba5fa0e8fd0f87373c15ebe86f
Valentin Calomino <vcalomino@fi.uba.ar>
2024-10-26 17:31:30 -0300

    add basic authentication with hardcoded user

--- src/main/java/com/ing_software/tp/config/SecurityConfig.java
+++ src/main/java/com/ing_software/tp/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.ing_software.tp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,9 +17,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(
-                request -> request.anyRequest().permitAll()
+                request -> request
+                        .requestMatchers("/register").permitAll()
+                        .requestMatchers("/prueba").authenticated()
         )
+                .httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("asd")
+                .password("asd")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
