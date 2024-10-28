3d81a06c998c81c7240748a52cea0e3ad2e43ad3
Valentin Calomino <vcalomino@fi.uba.ar>
2024-10-28 12:06:06 -0300

    handle login exceptions in global handler

--- src/main/java/com/ing_software/tp/advice/ApplicationExceptionHandler.java
+++ src/main/java/com/ing_software/tp/advice/ApplicationExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.ing_software.tp.advice;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,10 +16,22 @@ public class ApplicationExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException exception){
+    public Map<String, String> invalidRegisterRequest(MethodArgumentNotValidException exception){
         Map<String, String> errors = new HashMap<>();
         exception.getBindingResult().getFieldErrors().forEach(error ->
                 errors.put(error.getField(), error.getDefaultMessage()));
         return errors;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public String incorretLoginCredentialsException(UsernameNotFoundException exception){
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public String noBodyLoginRequestException(HttpMessageNotReadableException exception) {
+        return "The request requires a body with login credentials";
     }
 }
