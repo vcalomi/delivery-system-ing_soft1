8f3e7341a98b47c796a412faa9f8d579142dc544
Valentin Calomino <vcalomino@fi.uba.ar>
2024-10-29 00:49:15 -0300

    add frontend project

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 100vh; /* Asegura que el div principal ocupe el 100% de la altura */
+  display: flex; /* Permite centrar contenido con flexbox */
+  flex-direction: column; /* Apila los elementos verticalmente */
+  justify-content: center; /* Centra verticalmente */
+  align-items: center; /* Centra horizontalmente */
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #0b4f94;
+}
+</style>
---
+++ frontend/vue.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8098', // this configuration needs to correspond to the Spring Boot backends' application.properties server.port
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
+  outputDir: 'target/dist',
+  assetsDir: 'static'
+})
