7dea2142181c848f87c598fd95d2f9f750945952
unknown <lisandro.romanturrisi@gmail.com>
2024-10-30 18:59:21 -0300

    frontend configured to work along with backend. Register works fine. Login still fails bc of 401

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    port: 8080,
     proxy: {
       '/api': {
         target: 'http://localhost:8098', // this configuration needs to correspond to the Spring Boot backends' application.properties server.port
