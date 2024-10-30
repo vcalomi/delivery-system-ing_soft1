dc4f3f58674bb6f6fe97c5a8e394804dc960a1c3
Valentin Calomino <vcalomino@fi.uba.ar>
2024-10-29 23:22:44 -0300

    add entity classes for order and products

---
+++ src/main/java/com/ing_software/tp/model/Order.java
@@ -0,0 +1,22 @@
+package com.ing_software.tp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @OneToMany
+    private List<Product> products;
+}
