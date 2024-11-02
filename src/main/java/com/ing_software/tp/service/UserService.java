6844ff51778f64e088c590fa799a6163dc588953
Valentin Calomino <vcalomino@fi.uba.ar>
2024-11-01 23:26:41 -0300

    change register method now there's a separate method to save the user and to send the email

--- src/main/java/com/ing_software/tp/service/UserService.java
+++ src/main/java/com/ing_software/tp/service/UserService.java
@@ -4,11 +4,11 @@ import com.ing_software.tp.dto.LoginResponse;
 import com.ing_software.tp.dto.UserLoginRequest;
 import com.ing_software.tp.dto.UserRegisterRequest;
 import com.ing_software.tp.model.User;
+import jakarta.validation.Valid;
 
 public interface UserService {
 
     String registerUser(UserRegisterRequest user);
     LoginResponse loginUser(UserLoginRequest user);
-
     User findByUsername(String username);
 }
