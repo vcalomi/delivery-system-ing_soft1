05752a58b7a37e3e9104c2110db29ae9c1bc10e6
unknown <lisandro.romanturrisi@gmail.com>
2024-11-03 20:20:41 -0300

    new component for products and request created. Token hardcoded. To get token, we should register someone with postman

---
+++ frontend/src/components/ProductsComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <p>template</p>
+</template>
+<script>
+import axios from 'axios';
+export default{
+    name: 'ProductsComponent',
+    mounted:{
+        async createProducts(){
+            let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKb2huIn0.9Yg6ujs_hfsVQ2Xk1h7lRJ4wkWb2vGhWha8tBd_ko9k'
+            await axios.post('http://localhost:/api/products', {product_name: "product_1", stock: 20 }, {
+             headers: {Authorizaiton: "Bearer " + token } 
+            })
+            .then( res => console.log(res))
+            .catch( err => console.error(err))
+        }
+    },
+    methods:{
+        async createProducts(){
+            let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKb2huIn0.9Yg6ujs_hfsVQ2Xk1h7lRJ4wkWb2vGhWha8tBd_ko9k'
+            await axios.post('http://localhost:/api/products', {product_name: "product_1", stock: 20 }, {
+             headers: {Authorizaiton: "Bearer " + token } 
+            })
+            .then( res => console.log(res))
+            .catch( err => console.error(err))
+        }
+    }
+}
+</script>
