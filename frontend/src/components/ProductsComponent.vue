36c1f646d66829d1c0d9b359a6e29de74c018665
franzimbi <fzimbimbakis@fi.uba.ar>
2024-11-04 17:28:09 -0300

    comma bug solved

--- frontend/src/components/ProductsComponent.vue
+++ frontend/src/components/ProductsComponent.vue
@@ -26,16 +26,15 @@ export default{
         let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsaWNoYSJ9.7rNwjkBXI05IF8INBwQ0moNXuWPz6YGPvtQJOHTmJG4'
         console.log("Bearer " + token)
         await axios.get('http://localhost:8081/api/products/', {})
-            .then( res => console.log(res.data)
+            .then( res => console.log(res.data))
             .catch( err => console.error(err))
             
     },
-    
     methods:{
         async createProducts(){
             let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKb2huIn0.9Yg6ujs_hfsVQ2Xk1h7lRJ4wkWb2vGhWha8tBd_ko9k'
             await axios.post('http://localhost:8081/api/products/create', {product_name: "product_1", stock: 20 }, {
-             headers: {Authorizaiton: "Bearer " + token } 
+             headers: {Authorization: "Bearer " + token } 
             })
             .then( res => console.log(res))
             .catch( err => console.error(err))
