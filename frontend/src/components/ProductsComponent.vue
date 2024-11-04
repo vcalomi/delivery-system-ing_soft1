ecedfdea4d65e49feb82d1a47943c2dafe5fec79
unknown <lisandro.romanturrisi@gmail.com>
2024-11-04 19:54:35 -0300

    we can se a list of products and they stock

--- frontend/src/components/ProductsComponent.vue
+++ frontend/src/components/ProductsComponent.vue
@@ -1,5 +1,10 @@
 <template>
-    <p v-for=" p in productos" :key="p.id">{{p.product_name}}</p>
+    <ul  v-for="p in productos" :key="p.id">
+        <li v-if="p">product name: {{p.product_name}}, stock: {{ p.stock }}</li>
+
+    </ul>
+    
+    <button @click="getProducts">button</button>
 </template>
 <script>
 import axios from 'axios';
@@ -25,19 +30,16 @@ export default{
     async mounted()  {
         let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsaWNoYSJ9.7rNwjkBXI05IF8INBwQ0moNXuWPz6YGPvtQJOHTmJG4'
         console.log("Bearer " + token)
-        await axios.get('http://localhost:8081/api/products/', {})
-            .then( res => console.log(res.data))
-            .catch( err => console.error(err))
+        await axios.get('http://localhost:8081/api/products/', {}).then( res => {
+            this.productos = res.data
+            console.log(res.data)
+        })
+        .catch( err => console.error(err))
             
     },
     methods:{
-        async createProducts(){
-            let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKb2huIn0.9Yg6ujs_hfsVQ2Xk1h7lRJ4wkWb2vGhWha8tBd_ko9k'
-            await axios.post('http://localhost:8081/api/products/create', {product_name: "product_1", stock: 20 }, {
-             headers: {Authorization: "Bearer " + token } 
-            })
-            .then( res => console.log(res))
-            .catch( err => console.error(err))
+        getProducts(){
+            console.log(this.productos)
         }
     }
 }
