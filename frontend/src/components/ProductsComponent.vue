0871813d9fc2c30cec84912537de5de92df5b4d3
unknown <lisandro.romanturrisi@gmail.com>
2024-11-03 21:14:38 -0300

    token hardcoded. first needed to create an user then give him ADMIN role from database then you can start creating

--- frontend/src/components/ProductsComponent.vue
+++ frontend/src/components/ProductsComponent.vue
@@ -3,22 +3,23 @@
 </template>
 <script>
 import axios from 'axios';
+
 export default{
     name: 'ProductsComponent',
-    mounted:{
-        async createProducts(){
-            let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKb2huIn0.9Yg6ujs_hfsVQ2Xk1h7lRJ4wkWb2vGhWha8tBd_ko9k'
-            await axios.post('http://localhost:/api/products', {product_name: "product_1", stock: 20 }, {
-             headers: {Authorizaiton: "Bearer " + token } 
+    async mounted()  {
+        
+            let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsaWNoYSJ9.7rNwjkBXI05IF8INBwQ0moNXuWPz6YGPvtQJOHTmJG4'
+            console.log("Bearer " + token)
+            await axios.post('http://localhost:8081/api/products/create', {product_name: "product_2", stock: 20 }, {
+             headers: {Authorization: "Bearer " + token } 
             })
-            .then( res => console.log(res))
+            .then( res => console.log(res.status))
             .catch( err => console.error(err))
-        }
     },
     methods:{
         async createProducts(){
             let token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKb2huIn0.9Yg6ujs_hfsVQ2Xk1h7lRJ4wkWb2vGhWha8tBd_ko9k'
-            await axios.post('http://localhost:/api/products', {product_name: "product_1", stock: 20 }, {
+            await axios.post('http://localhost:8081/api/products/create', {product_name: "product_1", stock: 20 }, {
              headers: {Authorizaiton: "Bearer " + token } 
             })
             .then( res => console.log(res))
