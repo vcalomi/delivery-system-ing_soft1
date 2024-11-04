618e36b6817773a2b5d1fb39ca6b7edfd1608419
unknown <lisandro.romanturrisi@gmail.com>
2024-11-04 17:00:01 -0300

    bug when merging

--- src/test/java/com/ing_software/tp/controllerTests/UserLoginEndpointTest.java
+++ src/test/java/com/ing_software/tp/controllerTests/UserLoginEndpointTest.java
@@ -37,7 +37,8 @@ public class UserLoginEndpointTest {
     @BeforeAll
     static void registerUser(@Autowired TestRestTemplate restTemplate){
 
-        registeredUser = new User(null, "John", "Doe", "email@email.com", 20, "address", "John1", "1234", "USER");
+    	registeredUser = new User(null, "John", "Doe", "email@email.com", 20, "address", "John1", "1234", "USER");
+    }
 
 
     @Test
