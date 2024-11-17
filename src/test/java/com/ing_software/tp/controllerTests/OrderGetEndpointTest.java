12d4747fb31711ca06f42e5b52973098d1362bbc
Leonardo Dragone <leodragone1@gmail.com>
2024-11-17 17:34:43 -0300

    remove unnecessary imports

--- src/test/java/com/ing_software/tp/controllerTests/OrderGetEndpointTest.java
+++ src/test/java/com/ing_software/tp/controllerTests/OrderGetEndpointTest.java
@@ -11,8 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.*;
 import org.springframework.test.context.TestPropertySource;
 
